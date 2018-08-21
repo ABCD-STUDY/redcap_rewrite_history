b1a7f768bd645d2c3f84e0455f435444de91714d
Hauke Bartsch <HaukeBartsch@gmail.com>
2018-08-21 13:23:07 -0500

    first version of interface

--- RewriteHistory.php
+++ RewriteHistory.php
@@ -14,7 +14,7 @@ class RewriteHistory extends AbstractExternalModule {
 
     public function generateRewriteHistory() {
 
-        ?>
+?>
         <div style="text-align: left; width: 100%">
             <div style="height: 50px;"></div>
         </div>
@@ -23,32 +23,38 @@ class RewriteHistory extends AbstractExternalModule {
 
         <script src="<?= $this->getUrl("/rewriteHistory.js") ?>"></script>
 
-        <?php
+<?php
 
         $restrictedAccess = ($_REQUEST['page'] == 'executiveView' && (in_array(USERID, $this->getSystemSetting("executive-users")) || SUPER_USER) ? 1 : 0);
 	// do we have to set title here?
-        $title = "SOMETHING";
+        $title = "Rewrite History";
 	
-        ?>
-        <h2><?php echo($title); ?></h2>
-        <p>Lets start with a single item we want to rename.</p>
+?>
+
+
+  <h2><?php echo($title); ?></h2>
+  <p>Start with a single item we want to rename. Once this works we are 100% there...</p>
 
   <div class="form-group">
-    <label for="oldname">Old Name</label>
-    <input type="text" class="form-control" id="oldname" aria-describedby="oldNameHelp" placeholder="Enter name you want to replace">
+    <label for="oldname">Old Item Name</label>
+    <input type="text" class="form-control" id="oldname" aria-describedby="oldNameHelp" placeholder="Enter existing item name you want to replace">
     <small aria-describedby="oldNameHelp" class="form-text text-muted">This is for testing only - a spreadsheet renaming will likely follow.</small>
   </div>
 
   <div class="form-group">
-    <label for="newname">Old Name</label>
-    <input type="text" class="form-control" id="newname" aria-describedby="newNameHelp" placeholder="Enter name you want to replace">
+    <label for="newname">New Item Name</label>
+    <input type="text" class="form-control" id="newname" aria-describedby="newNameHelp" placeholder="Enter the new name">
     <small aria-describedby="newNameHelp" class="form-text text-muted">This is for testing only - a spreadsheet renaming will likely follow.</small>
   </div>
 
-  <textarea id="error-messages" title="debugging information"></textarea>
+  <div class="form-group">
+     <label><input type="checkbox" id="sure"> Are you sure?</label>&nbsp;
+     <button id="start-rewrite" class="btn btn-danger">Start rewrite</button>
+  </div>
 
-?>
+  <textarea id="error-messages" title="debugging information" rows="30" cols="90"></textarea>
+
 <?php
-
-
+    }
+}
 ?>
--- rewriteHistory.js
+++ rewriteHistory.js
@@ -1,6 +1,21 @@
-(function($, window, document) {
-    // sort table when document is loaded
-    $(document).ready(function () {
+jQuery(document).ready(function () {
 
+    jQuery('#start-rewrite').on('click', function() {
+	// only do something if the checkbox is on
+	var oldV = jQuery('#oldname').val();
+	var newV = jQuery('#newname').val();
+	if (oldV === "" || newV === "") {
+	    alert("Error: no value provided");
+	    return;
+	}
+	
+	if (jQuery('#sure').is(':checked')) {
+	    // replace
+	    console.log("rewrite now:" + jQuery('#oldname').val() + " -> " + jQuery('#newname').val());
+	} else {
+	    alert('Nothing will be done if you are not sure');
+	}
     });
-})(window.jQuery, window, document);
+    
+});
+
